2249589c0d874cb5bef87d482c0c993998f239a5
xyyx <xyyx@mail.ru>
2017-12-16 22:07:37 +0800

    mido: Import Lineage doze from op3 tree

--- device.mk
+++ device.mk
@@ -364,4 +364,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 PRODUCT_PACKAGES += \
+    XiaomiDoze \
     XiaomiParts
